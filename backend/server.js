5e788bd66b5cb58a9c3c1f778704b51f8b88940b
MartaJaszewska <jaszewska.marta@gmail.com>
2020-02-18 12:49:40 +0100

    code review

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '',
+    database : 'smart'
+  }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000')
+})
