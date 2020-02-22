164abeea2e83c0b91e91248162092565b49f63fa
MartaJaszewska <jaszewska.marta@gmail.com>
2020-02-22 15:28:11 +0100

    last fixes

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,17 +1,18 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    const { email, name, password } = req.body;
-    if (!email || !name || !password) {
-      return res.status(400).json('incorrect form submission');
-    }
-    const hash = bcrypt.hashSync(password, 10)
-    db.transaction(trx => {
-      trx.insert({
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  const hash = bcrypt.hashSync(password, 10);
+  db.transaction((trx) => {
+    trx
+      .insert({
         hash: hash,
         email: email
       })
       .into('login')
       .returning('email')
-      .then(loginEmail => {
+      .then((loginEmail) => {
         return trx('users')
           .returning('*')
           .insert({
@@ -19,16 +20,15 @@ const handleRegister = (req, res, db, bcrypt) => {
             email: loginEmail[0],
             joined: new Date()
           })
-          .then(user => {
-            res.json(user[0])
-          })
+          .then((user) => {
+            res.json(user[0]);
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-      .catch(err => res.status(400).json('unable to register'))
-}
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('unable to register'));
+};
 
 module.exports = {
-    handleRegister
-}
+  handleRegister
+};
--- frontend/src/components/Rank/index.js
+++ frontend/src/components/Rank/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Rank = ( {name, entries} ) => {
+const Rank = ({ name, entries }) => {
   return (
-    <div >
+    <div>
       {`${name} , your current entry count is...`}
-      <div className='white f1 '>
-        {entries}
-      </div>
+      <div className="white f1 ">{entries}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Rank;
