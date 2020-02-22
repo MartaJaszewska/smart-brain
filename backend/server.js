d44ee6eb35083e83a24848c8f7ee2fc427c16033
MartaJaszewska <jaszewska.marta@gmail.com>
2020-02-22 15:28:11 +0100

    last fixes

--- backend/server.js
+++ backend/server.js
@@ -6,27 +6,41 @@ const signin = require('./controllers/signin');
 const register = require('./controllers/register');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const PORT = process.env.PORT || 3000;
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'smart'
-  }
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'postgres',
+		database: 'smartbrain'
+	}
 });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
-app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) });
+app.get('/', (req, res) => {
+	res.send('Working fine.');
+});
+app.post('/signin', (req, res) => {
+	signin.handleSignin(req, res, db, bcrypt);
+});
+app.post('/register', (req, res) => {
+	register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfile(req, res, db);
+});
+app.put('/image', (req, res) => {
+	image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+	image.handleApiCall(req, res, db);
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+	console.log(`app is running on port ${PORT}`);
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
 import './index.css';
-import App from './containers/App/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
